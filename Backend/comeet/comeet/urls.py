f44a2a1a4783639de15a62a20f091f1ed47a6ce7
장덕인 <77999jno@kookmin.ac.kr>
2021-03-10 14:43:23 +0900

    S04P22A203-46 Backend Edit : Edit Swagger
    pattern 부분 수정

--- Backend/comeet/comeet/urls.py
+++ Backend/comeet/comeet/urls.py
@@ -21,9 +21,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# schema_url_patterns = [ 
-#     path('api/', include('test.urls')), 
-# ]
+schema_url_patterns = [ 
+    path('api/', include('api.urls')), 
+]
 
 schema_view = get_schema_view(
     openapi.Info( 
@@ -33,12 +33,12 @@ schema_view = get_schema_view(
         ), 
     public=True,
     permission_classes=(permissions.AllowAny,), 
-    # patterns=schema_url_patterns,
+    patterns=schema_url_patterns,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('test.urls')),
+    path('api/', include('api.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
