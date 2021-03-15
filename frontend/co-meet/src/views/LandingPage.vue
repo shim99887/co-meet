a9bbf7bbae9a8be7f265cb02afd0a3209a0c01a6
shim99887 <shim99887@naver.com>
2021-03-15 17:24:27 +0900

    S04P22A203-89 edit : Landing Page Animation add
    Landing Page 애니메이션 추가

--- frontend/co-meet/src/views/LandingPage.vue
+++ frontend/co-meet/src/views/LandingPage.vue
@@ -1,71 +1,152 @@
 <template>
   <v-container fluid>
-  <Navbar />
+    <Navbar @msg="getMsg"/>
     <video autoplay muted loop id="myVideo">
       <source src="../assets/sub2.mp4" type="video/mp4" />
     </video>
     <v-row no-gutters style="margin-top:60px">
       <v-col cols="5" class="ml-10">
         <div>
-          <div v-if="wid>1000" class="titles">
+          <div v-if="wid > 1000" class="titles">
             <p>CO-MEET</p>
           </div>
-          <div v-else style="position:absolute;font-size:80px; z-index:1;">
+          <div v-else class="mob_titles" style="">
             <p>CO-MEET</p>
           </div>
         </div>
       </v-col>
-      <v-col v-if="wid>1000" cols="4">
-        <user-form style="position:absolute;z-index:1; margin-top:250px;left:60%;" class="form" />
+      <v-col v-if="wid > 1000" cols="4">
+        <transition name="fade">
+          <user-form v-if="login" :msg="'login'" class="form" />
+          <user-form v-if="regist" :msg="'regist'" class="form" />
+        </transition>
       </v-col>
     </v-row>
-    <v-row style="margin-top:300px" v-if="wid<=1000">
-      <user-form/>
+    <v-row style="margin-top:300px;margin-left: 50px;" v-if="wid <= 1000">
+    <transition name="fade">
+      <user-form v-if="login"/>
+    </transition>
     </v-row>
   </v-container>
 </template>
 <script>
 import UserForm from "../components/UserForm.vue";
-import Navbar from "../components/navbar.vue"
+import Navbar from "../components/navbar.vue";
 
-export default ({
+export default {
   components: {
     UserForm,
     Navbar,
   },
-  data(){
-    return{
-      wid:window.innerWidth,
+  data() {
+    return {
+      wid: window.innerWidth,
+      login: false,
+      regist: false,
     };
   },
-  created(){
+  created() {
     window.addEventListener("resize", this.resizeEventHandler);
   },
-  destroyed(){
+  destroyed() {
     window.removeEventListener("resize", this.resizeEventHandler);
   },
-  methods:{
-    resizeEventHandler(event){
+  methods: {
+    resizeEventHandler(event) {
       // console.log(event);
       this.wid = event.target.innerWidth;
     },
+    getMsg(msg){
+      if(msg == 'login'){
+        console.log('1');
+        this.login= !this.login;
+        this.regist=false;
+      }else{
+        console.log('2');
+        this.regist = !this.regist;
+        this.login = false;
+      }
+    }
   },
-});
+};
 </script>
 <style scoped>
 body {
   margin: 0px;
 }
-.titles{
-  position:absolute;
-  font-size:10rem;
-  color:#22223b;
-  opacity:0.75;
-  text-shadow: 4px 4px 4px gray;
-  z-index:1; 
+.fade-enter-active{
+  /* transition: opacity .1s; */
+  animation: anim3 forwards 2s;
+}
+ .fade-leave-active {
+   animation: anim5 forwards 2s;
+ }
+@keyframes anim5{
+  from{
+    bottom:3%;
+  }
+  to{
+    bottom:-100%;
+  }
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.form {
+  position: absolute;
+  z-index: 1;
+  /* margin-top: 250px; */
+  bottom:3%;
+  left: 60%;
+}
+.titles {
+  position: absolute;
+  font-size: 10rem;
+  /* color: #22223b;
+  opacity: 0.75;
+  text-shadow: 4px 4px 4px gray; */
+  color : #ffa38c;
+  text-shadow: 4px 4px 4px rgb(214, 107, 107);
+  z-index: 1;
   font-family: "Bahnschrift Condensed";
-  margin-left:50px; 
-  margin-top:150px;
+  margin-left: 50px;
+  margin-top: 150px;
+  bottom: -100%;
+  animation: anim 2s forwards 2s;
+}
+.mob_titles {
+  animation: anim2 2s forwards 2s;
+  bottom: -100%;
+  position: absolute;
+  font-size: 6rem;
+  color: #22223b;
+  opacity: 0.75;
+  text-shadow: 4px 4px 4px gray;
+  z-index: 1;
+}
+@keyframes anim {
+  from {
+    bottom: -100%;
+  }
+  to {
+    bottom: 30%;
+  }
+}
+@keyframes anim2 {
+  from {
+    bottom: -100%;
+  }
+  to {
+    bottom: 60%;
+  }
+}
+@keyframes anim3 {
+  from {
+    bottom: -100%;
+  }
+  to {
+    bottom: 3%;
+  }
 }
 #myVideo {
   object-fit: cover;
@@ -75,5 +156,4 @@ body {
   top: 0;
   left: 0;
 }
-
 </style>
