2fcd9603038fc7cbca3f568c9d0eab6dda4fb01b
최낙훈 <cnh6123@naver.com>
2021-03-18 00:03:42 +0900

    S04P22A203-114 Backend edit : corona data crawling discharge column data hotfix
    코로나 데이터 discharge 컬럼 데이터 핫픽스

--- Backend/comeet/corona_ver2.py
+++ Backend/comeet/corona_ver2.py
@@ -20,7 +20,7 @@ def get_seoul_covid19_100(page_no):
     start_no : 입력받은 page_no
     """
     start_no = (page_no - 1) * 100
-    # url = f"https://news.seoul.go.kr/api/27/getCorona19Status/get_status_ajax_pre.php?draw={page_no}"
+    #url = f"https://news.seoul.go.kr/api/27/getCorona19Status/get_status_ajax_pre.php?draw={page_no}"
     # 최신 데이터
     url = f"https://news.seoul.go.kr/api/27/getCorona19Status/get_status_ajax.php?draw={page_no}"
     url = f"{url}&order%5B0%5D%5Bdir%5D=desc&start={start_no}&length=100"
@@ -40,10 +40,10 @@ def get_multi_page_list(start_page, end_page=80):
             one_page = pd.DataFrame(one_page["data"], columns=[
                                     'serial_number', 'patient_number', 'date', 'dong', 'overseas', 'route', 'discharge'])
 
-            # 어제 날짜를 yesterday 변수에 저장합니다.
+            # # 어제 날짜를 yesterday 변수에 저장합니다.
             yesterday = datetime.today() - timedelta(1)
 
-            # 어제 날짜에 해당되는 데이터만 filter 처리합니다.
+            # # 어제 날짜에 해당되는 데이터만 filter 처리합니다.
             one_page = one_page[one_page.date ==
                                 yesterday.strftime('%Y-%m-%d')]
 
@@ -51,7 +51,7 @@ def get_multi_page_list(start_page, end_page=80):
             one_page["serial_number"] = one_page["serial_number"].apply(lambda e: int(e.replace(
                 "<p class='corona19_no'>", "").replace("</p>", "")))
             one_page["discharge"] = one_page["discharge"].apply(lambda e: e.replace(
-                "<b class='status2'>", "").replace("</b>", "").replace("<b class='status1'>", "").replace("<b class=''>", ""))
+                "<b class='status2'>", "").replace("</b>", "").replace("<b class='status1'>", "").replace("<b class=''>", "-"))
             page_list.append(one_page)
             # 서버에 한번에 너무 많은요청을 보내면 서버에 부담이 됩니다.
             # 서버에 부담을 주지 않기 위애 0.5초씩 쉬었다 가져옵니다.
@@ -80,7 +80,7 @@ records_total = data_json['recordsTotal']
 start_page = 1
 
 # 전체 기준 페이지 수
-# end_page = round(records_total / 100) + 1
+#end_page = round(records_total / 100) + 1
 
 # 날짜 별 기준 페이지 수
 end_page = 5
