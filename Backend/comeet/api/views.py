34f24ad67a15c9ddcb55cf4263b047b643654ce2
강세준 <dhpassion@naver.com>
2021-03-23 21:05:54 +0900

    S04P23A203-8 BE/redis edit : ascii transform
    한글꺠짐현상 해결

--- Backend/comeet/api/views.py
+++ Backend/comeet/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework import status, viewsets, mixins 
-from rest_framework.response import Response 
-from django.views import View 
+from rest_framework import status, viewsets, mixins
+from rest_framework.response import Response
+from django.views import View
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
@@ -10,57 +10,62 @@ from .serializers import CodeSerializer, FpoplSerializer, CardSerializer, Corona
 from django.core.cache import cache
 # Create your views here.
 
-class CoronaSet(viewsets.GenericViewSet, mixins.ListModelMixin, View) :
+
+class CoronaSet(viewsets.GenericViewSet, mixins.ListModelMixin, View):
     serializer_class = CoronaDataSerializer
-                
+
     def set_corona(self, *args, **kwargs):
-        # queryset data 받기 
+        # queryset data 받기
         corona_data = CoronaData.objects.all()
         if not corona_data.exists():
             raise HttpResponse()
 
-        # json 파일로 변환 
+        # json 파일로 변환
         corona_data_list = serializers.serialize('json', corona_data)
-        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로 
+        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로
         cache.set("corona_data", corona_data, 24 * 60 * 60)
-        
-        return JsonResponse({"message" : "CORONA_SUCCESS"}, status = 200)
 
-class CodeSet(viewsets.GenericViewSet, mixins.ListModelMixin, View) :
+        return JsonResponse({"message": "CORONA_SUCCESS"}, status=200)
+
+
+class CodeSet(viewsets.GenericViewSet, mixins.ListModelMixin, View):
     serializer_class = CodeSerializer
-         
+
     def set_code(self, *args, **kwargs):
         code_data = Code.objects.all()
 
-       # json 파일로 변환 
+       # json 파일로 변환
         code_data_list = serializers.serialize('json', code_data)
-        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로 
+        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로
         cache.set("code_data", code_data, 24 * 60 * 60)
 
-        return JsonResponse({'message' : 'CODE_SUCCESS'},status=200)
+        return JsonResponse({'message': 'CODE_SUCCESS'}, status=200)
 
-class FpoplSet(viewsets.GenericViewSet, mixins.ListModelMixin, View) :
+
+class FpoplSet(viewsets.GenericViewSet, mixins.ListModelMixin, View):
     serializer_class = FpoplSerializer
 
     def set_fpopl(self, *args, **kwargs):
         fpopl_data = Fpopl.objects.all()
 
-       # json 파일로 변환 
+       # json 파일로 변환
         fpopl_data_list = serializers.serialize('json', fpopl_data)
-        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로 
+        # # 데이터를 하루동안 저장, 자르기 편하게 queryset 형식으로
         cache.set("fpopl_data", fpopl_data, 24 * 60 * 60)
 
-        return JsonResponse({'message' : 'FPOPL_SUCCESS'},status=200)
+        return JsonResponse({'message': 'FPOPL_SUCCESS'}, status=200)
 
-class CoronaList(viewsets.GenericViewSet, mixins.ListModelMixin, View) :
+
+class CoronaList(viewsets.GenericViewSet, mixins.ListModelMixin, View):
     serializer_class = CoronaDataSerializer
 
-    def get_corona_list(self, *args, **kwargs): 
+    def get_corona_list(self, *args, **kwargs):
         corona_queryset = cache.get("corona_data")
         # 중구 데이터 출력 query가 utf-8 잘되었는지 확인
-        print(corona_queryset.filter(gugun="중구").values())
-        
-        corona_json = serializers.serialize('json', corona_queryset, ensure_ascii=True)
-        # print(corona_json)
+        # print(corona_queryset.filter(gugun="중구").values())
+
+        corona_json = serializers.serialize(
+            'json', corona_queryset, ensure_ascii=False)
+        print(corona_json)
         # return JsonResponse(corona_json, safe= False)
         return None
