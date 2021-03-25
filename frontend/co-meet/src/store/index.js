bd3bd4748e56e039a9d67867807f365ec64293e4
hsy118 <hsy118@hotmail.co.kr>
2021-03-25 18:37:01 +0900

    S04P23A203-43 FE/EDIT: make map and chart reactive
    지도, 그래프 추가 마커 추가, 일단 지역 하나 받아와서 마커로 넣고
    그 찍은 마커로 flyto기능 구현

--- frontend/co-meet/src/store/index.js
+++ frontend/co-meet/src/store/index.js
@@ -1,17 +1,86 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    searchingCity: "",
+    onSearching: false,
+    gugun: [],
+    gugunData: [],
+    recomCity: [],
+    mapToggle: false,
   },
   getters:{
+    get_gugun(state) {
+      return state.gugun
+    },
+    get_gugunData(state) {
+      return state.gugunData
+    },
+    get_city(state) {
+      return state.searchingCity
+    },
+    get_result(state) {
+      return state.recomCity
+    },
+    get_mapToggle(state) {
+      return state.mapToggle
+    }
   },
   mutations: {
+    MAPTOGGLE(state) {
+      state.mapToggle = !state.mapToggle
+    },
+    ON_SEARCHING(state) {
+      state.onSearching = true
+    },
+    OFF_SEARCHING(state) {
+      state.onSearching = false
+    },
+    PUT_CITY(state, city) {
+      state.searchingCity = city
+    },
+    PUT_RESULT(state, location) {
+      state.recomCity.push(location)
+    },
   },
   actions: {
+    async GET_RECOM(context, city) {
+      try {
+        const res = await axios.post("https://j4a203.p.ssafy.io/api/recomm", {signgu_nm: city})
+        context.commit('PUT_CITY', city)
+        const data = res.data
+        const location = {
+          lat: data.recomm_lat,
+          lng: data.recomm_lng,
+          gu: data.signgu_nm,
+        }
+        context.commit("PUT_RESULT", location)
+        context.commit("MAPTOGGLE")
+      } catch(err) {
+        console.log(err)
+      }
+    },
+    async GET_CORONA_PER_CITY(context) {
+      try {
+        const res = await axios.get("https://j4a203.p.ssafy.io/api/corona-list")
+        const data = res.data
+        for (let key in data.gugun) {
+          this.state.gugun.push(data.gugun[key])
+        }
+        for (let key in data.serial_number) {
+          this.state.gugunData.push(data.serial_number[key])
+        }
+        console.log(this.state.gugun)
+        console.log(this.state.gugunData)
+        context.commit("OFF_SEARCHING")
+        
+      } catch (err) {
+        console.log(err)
+      }
+    },
   },
-  modules: {
-  }
 })
